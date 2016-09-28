b7997f5ed843125a4d8cea46e7906b09ffbdc526
Chris Mannel <cmannel77@gmail.com>
2016-09-28 09:53:26 -0500

    Added Routes prior to course ctrl updates
    Service to update DB not working
    
    Going to update ctrl and svc modules to match course content files to
    see if will then work.

--- src/api/index.js
+++ src/api/index.js
@@ -15,7 +15,7 @@ router.get('/todos', function (req, res) {
   });
 });
 
-//TODO: Add POST route to create new entries
+
 router.post('/todos', function (req, res) {
   var todo = req.body;
   Todo.create(todo, function (err, todo) {  //Todo model create method
@@ -26,7 +26,20 @@ router.post('/todos', function (req, res) {
   });
 });
 
-//TODO: Add PUT route to update existing entries
+router.put('/todos/:id', function (req, res) {
+  var id = req.params.id;
+  var todo = req.body;
+  if (todo && todo._id !== id) {
+    return res.status(500).json({err: "Ids don't match"});
+  }
+  Todo.findByIdAndUpdate(id, todo, {new: true}, function (err, todo) {  //Todo model update method
+    if (err) {
+      return res.status(500).json({err: err.message}); //err handling
+    }
+    res.json({'todo': todo, message: 'Todo Updated'});
+  });
+});
+
 
 //TODO: Add DELETE route to delete existing entries
 
