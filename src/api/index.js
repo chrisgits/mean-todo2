b8e1faea8b2783eced80e5226119a264a5a5ee30
Chris Mannel <cmannel77@gmail.com>
2016-09-27 21:53:15 -0500

    Setup mongoose connections
    create mongoose Schema
    
    set model with mongoose model method
    
    restructure get route
    
    setup post route

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,30 @@
 'use strict';
 
 var express = require('express');
-var todos = require('../../mock/todos.json');
+var Todo = require('../models/todo');
+//var todos = require('../../mock/todos.json');
 
 var router = express.Router();
 
 router.get('/todos', function (req, res) {
-  res.json({'todos': todos}); // serves json response of mock data
+  Todo.find({}, function (err, todos) { //pulls Todo model using find() method
+    if (err) {
+      return res.status(500).json({message: err.message});
+    }
+    res.json({'todos': todos});
+  });
 });
 
 //TODO: Add POST route to create new entries
+router.post('/todos', function (req, res) {
+  var todo = req.body;
+  Todo.create(todo, function (err, todo) {  //Todo model create method
+    if (err) {
+      return res.status(500).json({err: err.message}); //err handling
+    }
+    res.json({'todo': todo, message: 'Todo Created'});
+  });
+});
 
 //TODO: Add PUT route to update existing entries
 
